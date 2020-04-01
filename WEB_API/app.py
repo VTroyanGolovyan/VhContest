34e3b41cc552e15781fc40a68f319feda0d771bf
Vladislav <vladislavhacker111@gmail.com>
2020-04-01 21:17:16 +0300

    api routes

--- WEB_API/app.py
+++ WEB_API/app.py
@@ -1,12 +1,45 @@
 from flask import Flask
+import json
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def main():
+    return 'Home route'
+
+
+@app.route('/sign/in')
+def sign_in():
+    return 'Sign in route!'
+
+
+@app.route('/sign/up')
+def sign_up():
+    return 'Sign up route!'
+
+
+@app.route('/sign/out')
+def sign_out():
+    return 'Sign out route!'
+
+
+@app.route('/archive')
+def archive():
+    return 'Tasks archive route'
+
+
+@app.route('/task')
+def task():
     return 'Hello World!'
+
+
+@app.route('/check')
+def task():
+    return 'Task check!'
 
 
 if __name__ == '__main__':
     app.run()
+
