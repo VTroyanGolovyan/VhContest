8fe4ee54cc47ef9579ef40b7c9b2872a0b357196
Vladislav <vladislavhacker111@gmail.com>
2020-04-05 06:54:05 +0300

    Running manual

--- WEB_API/db_models.py
+++ WEB_API/db_models.py
@@ -10,5 +10,46 @@ class User(db.Model):
     email = db.Column(db.String(200), unique=False, nullable=False)
     password = db.Column(db.String(300), unique=False, nullable=False)
     salt = db.Column(db.String(10), unique=False, nullable=False)
+
     def __repr__(self):
-        return '<User %r>' % self.name
+        pass
+
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
+
+    def __repr__(self):
+        pass
+
+
+class Test(db.Model):
+    __tablename__ = 'tests'
+    id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, unique=False, nullable=False)
+    input = db.Column(db.Text, unique=False)
+    output = db.Column(db.Text, unique=False)
+
+    def __repr__(self):
+        pass
+
+
+class TestResult(db.Model):
+    __tablename__ = 'tests_results'
+    id = db.Column(db.Integer, primary_key=True)
+    sending_id = db.Column(db.Integer, unique=False)
+    test_id = db.Column(db.Integer, unique=False)
+    output = db.Column(db.String(20), unique=False)
+
+    def __repr__(self):
+        pass
+
+class Sending(db.Model):
+    __tablename__ = 'sendings'
+    id = db.Column(db.Integer, primary_key=True)
+    sending_id = db.Column(db.Integer, unique=False)
+    test_id = db.Column(db.Integer, unique=False)
+    output = db.Column(db.String(20), unique=False)
+
+    def __repr__(self):
+        pass
