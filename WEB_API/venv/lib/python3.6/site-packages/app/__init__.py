d43220936e17fd83243b36f735be48d04c424b57
Vladislav <vladislavhacker111@gmail.com>
2020-04-18 08:16:22 +0300

    trash fix

--- WEB_API/venv/lib/python3.6/site-packages/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-app.config.from_object("config")
-
-from app.module_one.controllers import module_one
-
-app.register_blueprint(module_one)
